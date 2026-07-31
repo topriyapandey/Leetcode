b1556543f2ef632c224026190e740cfdb0eba0fe
Priya Pandey <work.priyafeb2005@gmail.com>
2026-07-31 09:31:55 +0530

    Time: 189 ms (85.18%), Space: 10.5 MB (64.74%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool dfs(vector<vector<char>>& board, string &word, int i, int j, int k){
+        if(k==word.size())
+            return true;
+
+        if(i<0||j<0||i>=board.size()||j>=board[0].size()||board[i][j]!=word[k])
+            return false;
+
+        char ch=board[i][j];
+        board[i][j]='#';
+
+        bool found=dfs(board,word,i+1,j,k+1)||
+                   dfs(board,word,i-1,j,k+1)||
+                   dfs(board,word,i,j+1,k+1)||
+                   dfs(board,word,i,j-1,k+1);
+
+        board[i][j]=ch;
+
+        return found;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int m=board.size(),n=board[0].size();
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(dfs(board,word,i,j,0))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
