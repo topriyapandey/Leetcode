5466cbb0e0512a92f6412f40bfac258d2008a861
Priya Pandey <work.priyafeb2005@gmail.com>
2026-08-04 12:28:11 +0530

    Time: 0 ms (100%), Space: 97.5 MB (7.17%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int mn = INT_MAX;
+        int profit = 0;
+
+        for(int i = 0; i < prices.size(); i++) {
+
+            if(prices[i] < mn) {
+                mn = prices[i];
+            }
+            else if(prices[i] - mn > profit) {
+                profit = prices[i] - mn;
+            }
+        }
+
+        return profit;
+    }
+};
