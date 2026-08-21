fb5c4898fdf6e35da1b2f5aa7742aec30af12311
Priya Pandey <work.priyafeb2005@gmail.com>
2026-08-21 10:44:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.22%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2)
+            return x;
+
+        long long low = 1;
+        long long high = x;
+        int ans = 0;
+
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+
+            if (mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
