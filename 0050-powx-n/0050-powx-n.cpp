409db204d71c5fbce85ec28edb27dae09c40d2e9
Priya Pandey <work.priyafeb2005@gmail.com>
2026-09-04 07:54:39 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.73%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long power = n;
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double result = 1;
+
+        while (power > 0) {
+            if (power & 1) {
+                result *= x;
+            }
+
+            x *= x;
+            power >>= 1;
+        }
+
+        return result;
+    }
+};
